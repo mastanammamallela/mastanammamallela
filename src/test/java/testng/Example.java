1316934c18990ad44863c22d18c3439fbd1f3603
masta <masta@mastanamma>
2023-08-07 15:03:51 +0530

    sir,I develop new script

---
+++ src/test/java/testng/Example.java
@@ -0,0 +1,19 @@
+package testng;
+
+import org.testng.annotations.Test;
+
+public class Example {
+	public class Testng {
+		@Test(priority=1)
+		public void test1() {
+
+		System.out.println("hai");
+		}
+		@Test(priority=2)
+		public void test2() {
+		System.out.println("my name is mastanamma");	
+		}
+	}
+
+
+}
